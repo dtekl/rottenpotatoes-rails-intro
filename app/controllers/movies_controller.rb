fda4b0858eabafd60a0d8f3985c00367bf167dc6
Dtek <dhindsill@gmail.com>
2017-11-20 01:45:23 +0000

    part 3 done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,17 +11,68 @@ class MoviesController < ApplicationController
   end
 
   def index
-    my_params = params.permit(:column, :ratings)
+    params.permit(:column, :ratings)
     sort_col = ""
     @title_sty = nil
     @date_sty = nil
     @all_ratings = Movie.get_ratings
-    @checked = params[:ratings]
-    puts @checked
+    
+    ###############
+    #check first what's come in from params
+    #if if no column, then fill with session (if session)
+    #if param[:ratings]
+    #if session[:ratings] = param[:ratings] do nothing
+    #if !session[:ratings] or session[:ratings] != paraa [:ratings]
+    #session[:ratings] = param[:ratings].
+    
+    #first check if no params[:column] and params[:ratings]
+    #set vars from session and redirect with session params
+    
+    col_param = params[:column]
+    ratings_param = params[:ratings]
+    col_session = session[:column]
+    ratings_session = session[:ratings]
+    if !ratings_session or (!params[:ratings]  and params[:commit] == "Refresh")
+      ratings_session = {}
+      Movie.get_ratings.each {|s| ratings_session[s] = 1}
+      session[:ratings] = ratings_session
+    end
+    
+    if !col_session
+      col_session = ''
+      session[:column] = ''
+    end
    
     
-    if my_params[:column]
-      sort_col = my_params[:column]
+    if !col_param and !ratings_param #redirect with params just whats in the session
+      flash.keep
+      redirect_to movies_path(:column => col_session, :ratings => ratings_session) and return
+    end
+    
+    if !ratings_param #i.e. no ratings parameter passed so use session 
+      session[:ratings] = ratings_param
+      ratings = ratings_session
+      flash.keep
+      redirect_to movies_path(:column => col_param, :ratings => ratings) and return
+    end
+    
+    if !col_param #i.e. no column parameter passed so use session
+      session[:column] = col_param
+      
+      sort_col = col_session
+      ratings = ratings_param
+      flash.keep
+      redirect_to movies_path(:column => sort_col, :ratings => ratings) and return
+    end
+      
+  
+   if col_param and ratings_param
+     session[:column] = col_param
+     session[:ratings] = ratings_param
+   end
+    
+    if col_param
+      sort_col = params[:column]
     end
     
     if sort_col == 'title'
@@ -32,12 +83,10 @@ class MoviesController < ApplicationController
       @date_sty = 'hilite'
     end
     
-    if params[:ratings]
-      puts "Have some filters"
-      @movies = Movie.where(:rating => params[:ratings].keys)
-    else  
-      @movies = Movie.all.order(sort_col)
-    end
+   
+     
+    @movies = Movie.where(:rating => ratings_param.keys).order(sort_col)
+    
    
   end
 
